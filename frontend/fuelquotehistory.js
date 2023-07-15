a734a1ff3a39d2daef9bf342f3ebfe8f862ad170
francisvtran <francisvtran@gmail.com>
2023-07-14 23:44:36 -0500

    Update fuelquotehistory.js

---
+++ frontend/fuelquotehistory.js
@@ -0,0 +1,5 @@
+//Receive value from fuel quote form page
+var b = localStorage.getItem("myValue");
+alert("The Value Received is " + b);
+var resetValue =0;
+localStorage.setItem("myValue", resetValue);   
